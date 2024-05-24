301d01d5db50092f62e66e76ecd7a54f746b5872
Philipp66904 <philippg.pgb@gmail.com>
2024-05-24 10:49:42 +0200

    updated serial mount

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker run thi_drone_software --privileged -v /run/udev:/run/udev -v /dev/ttyAMA0:/dev/serial0  --network host
+docker run thi_drone_software --privileged -v /run/udev:/run/udev -v /dev/ttyAMA10:/dev/serial0  --network host
