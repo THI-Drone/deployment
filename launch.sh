b9a22bc65eae67a1f0380569b65dcb627948f72d
Philipp66904 <philippg.pgb@gmail.com>
2024-05-24 10:47:15 +0200

    updated launch.sh

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 source /opt/ros/humble/setup.bash
 source /uav/thi-drone-ws/install/setup.bash
-ros2 launch mission_software_launch launch.py
+ros2 launch mission_software_launch launch.py UAV_ID:=UAV_FFI_4
