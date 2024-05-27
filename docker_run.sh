4501aacf0dda9cea9174197037ff2aa7436371f1
Philipp66904 <philippg.pgb@gmail.com>
2024-05-27 17:03:35 +0200

    updated files

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker run --privileged -v /run/udev:/run/udev -v /dev/serial0:/dev/serial0  -it --network host --rm thi_drone_software "$@"
+docker run --privileged -v /run/udev:/run/udev -v /dev/serial0:/dev/serial0 -v /home/pi5/deployment/mission_files:/uav/mission_files -v ${PWD}/log:/log -v ${PWD}/bag:/bag -it --network host --rm thi_drone_software "$@"
