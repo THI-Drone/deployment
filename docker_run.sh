d96b5bc8812bee00634af02c11979e98ff7b71be
Philipp66904 <philippg.pgb@gmail.com>
2024-05-29 18:18:49 +0200

    docker run serial device renamed

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker run --privileged -d -v /run/udev:/run/udev -v /dev/serial0:/dev/serial0 -v /home/pi5/deployment/mission_files:/uav/mission_files -v ${PWD}/log:/log -v ${PWD}/bag:/bag -it --network host --rm thi_drone_software "$@"
+docker run --privileged -d -v /run/udev:/run/udev -v /dev/fcc_uart:/dev/fcc_uart -v /home/pi5/deployment/mission_files:/uav/mission_files -v ${PWD}/log:/log -v ${PWD}/bag:/bag -it --network host --rm thi_drone_software "$@"
