9ba8587c2c83f0b1c1b1bba1be44e12c425c9459
Finn Ehlers <finndows@outlook.de>
2024-05-31 18:37:48 +0200

    added mounting of /images

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker run --privileged -d -v /run/udev:/run/udev -v /dev/fcc_uart:/dev/fcc_uart -v /home/pi5/deployment/mission_files:/uav/mission_files -v ${PWD}/log:/log -it --network host --rm thi_drone_software "$@"
+docker run --privileged -d -v /run/udev:/run/udev -v /dev/fcc_uart:/dev/fcc_uart -v /home/pi5/deployment/mission_files:/uav/mission_files -v ${PWD}/log:/log -v ${PWD}/images:/images -it --network host --rm thi_drone_software "$@"
