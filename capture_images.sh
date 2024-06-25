02aac9e294b3169ce56df0044e870518f100d23b
J0las <52259564+J0las@users.noreply.github.com>
2024-06-25 13:07:21 +0200

    Update capture_images.sh

--- capture_images.sh
+++ capture_images.sh
@@ -1,3 +1,4 @@
 DATE=$(date -u +"%Y-%m-%d_%H-%M-%S")
 mkdir -p timelapse/$DATE
-rpicam-still --timeout 1800000 --timelapse 1000 -o timelapse/$DATE/image%04d.jpg
+ln -sf $DATE timelapse/latest
+nohup rpicam-still --timeout 1800000 --timelapse 1000 -o timelapse/$DATE/image%04d.jpg 2>/dev/null & 
